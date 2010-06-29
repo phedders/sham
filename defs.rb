d1d74152850019b756932643eabe2dbcc558dfc5
Paul Hedderly <paul+git@mjr.org>
2010-06-29 14:17:57 +0100

    prh: Import sham to new branch to drop old history for various reasons. History may be accessed on request

---
+++ defs.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+#
+# This file contains ruby code
+# It's purpose is to collect test/filter/audit routines.
+# def's can also be put in repodir:recipes/*.rb and ~/.sham-*.rb and /etc/sham/*.rb
+
+# eg
+#
+#def audit_date(data, old_data, *args)
+#				puts data
+#end
